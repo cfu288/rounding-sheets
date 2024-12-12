e136d13de41b980815091035c72e517fa47bc173
Christopher Fu <invent288cfu@gmail.com>
2024-12-12 16:56:07 -0500

    feat: update homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,14 @@
 import { display_templates } from "./const";
 
+const gradients = [
+  // "from-teal-600/40 via-indigo-800/20",
+  "from-violet-600/40 to-indigo-600/20",
+  "from-amber-500/40 to-pink-500/20",
+  "from-fuchsia-500/40 to-cyan-500/20",
+  "from-rose-400/40 to-red-500/20",
+  "from-amber-200/40 to-yellow-500/20",
+];
+
 export const Home = () => {
   return (
     <div className="bg-white py-24 sm:py-32">
@@ -9,11 +18,22 @@ export const Home = () => {
             ReverbMD
           </h2>
           <p className="mt-2 text-lg/8 text-gray-600">
-            Custom scutsheets and clinical tools
+            Explore our custom scutsheets built to help you stay organized and a
+            variety of clinical tools to help fill in the missing feature gaps
+            of your EMR.
           </p>
         </div>
-        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {display_templates.map((template) => (
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-pretty text-3xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+            Scutsheets
+          </h2>
+          <p className="mt-2 text-lg/8 text-gray-600">
+            Use one of our premade scutsheets to help you stay organized, or
+            customize a template to make it your own.
+          </p>
+        </div>
+        <div className="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {display_templates.map((template, index) => (
             <article
               key={template.templateId}
               className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
@@ -24,6 +44,11 @@ export const Home = () => {
                 className="absolute inset-0 -z-10 size-full object-cover"
               />
               <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+              <div
+                className={`absolute inset-0 -z-10 bg-gradient-to-br ${
+                  gradients[index % gradients.length]
+                }`}
+              />
               <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
               <h3 className="mt-3 text-lg/6 font-semibold text-white">
                 <a href={`/scutsheet/${template.templateId}`}>
@@ -38,15 +63,30 @@ export const Home = () => {
           ))}
         </div>
         <div className="mt-16">
-          <h2 className="text-xl font-semibold mb-2">Tools</h2>
-          <div className="grid grid-cols-1 gap-4">
-            <a
-              href="/tools/some_tool"
-              className="block p-4 bg-white shadow-md rounded-lg hover:bg-gray-100 w-full sm:w-96 mx-auto"
-            >
-              <h3 className="text-lg font-medium">Tool Name</h3>
-              <p className="text-sm text-gray-600">Description of the tool.</p>
-            </a>
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-pretty text-3xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+              Tools
+            </h2>
+            <p className="mt-2 text-lg/8 text-gray-600">
+              Helpful utilities to get things done.
+            </p>
+          </div>
+          <div className="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+              <div
+                className={`absolute inset-0 -z-10 bg-gradient-to-t ${
+                  gradients[Math.floor(Math.random() * gradients.length)]
+                }`}
+              />
+              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+              <h3 className="mt-3 text-lg/6 font-semibold text-white">
+                <a href="/tools/some_tool">
+                  <span className="absolute inset-0" />
+                  Tools are coming soon!
+                </a>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm/6 text-gray-300"></p>
+            </article>
           </div>
         </div>
       </div>
