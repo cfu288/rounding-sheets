9ab5bfd85ff6e40e587ff917c206d655b66ead2d
Christopher Fu <invent288cfu@gmail.com>
2024-12-16 20:37:28 -0500

    feat: update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -129,7 +129,7 @@ export const Home = () => {
               ))}
             </div>
           </div>
-          <div id="tools" className="mt-12 mb-16">
+          <div id="tools" className="mt-12 pb-24">
             <div className="border-b border-gray-200 pb-5">
               <h3 className="text-base font-semibold text-gray-900">Tools</h3>
               <p className="mt-2 max-w-4xl text-sm text-gray-500">
